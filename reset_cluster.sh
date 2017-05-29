6bf29d9c97413eaf6f22157db806759cee6ba883
caiopo <caiopoliveira@gmail.com>
2017-05-29 13:36:44 -0300

    Add fw1 to reset_cluster.sh

--- reset_cluster.sh
+++ reset_cluster.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc fw1 --replicas=0
 sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc raft --replicas=0
 sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc logger --replicas=0
 
@@ -7,3 +8,4 @@ sleep 10
 
 sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc logger --replicas=$1
 sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc raft --replicas=3
+sudo /opt/bin/kubectl --server=192.168.1.200:8080 scale rc fw1 --replicas=1
