22cc87ac809604b8827509f3a87a888fe70bb6ff
caiopo <caiopoliveira@gmail.com>
2017-05-29 13:19:49 -0300

    Change build script image tag

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 go build -ldflags "-linkmode external -extldflags -static"
-docker build -t caiopo/raft .
+docker build -t caiopo/raft:latest .
 rm raft
-docker push caiopo/raft
+docker push caiopo/raft:latest
